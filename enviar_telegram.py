3a402791017913f137d88357a2c0118c7cd895c2
EliasSantos47 <elias.sup@gmail.com>
2026-01-24 17:18:18 -0300

    Add files via upload

--- enviar_telegram.py
+++ enviar_telegram.py
@@ -1,16 +1,18 @@
 import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT
 
-def enviar(mensagem):
+def enviar(msg):
+    print("TOKEN:", TELEGRAM_TOKEN)
+    print("CHAT:", TELEGRAM_CHAT)
+
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
     payload = {
         "chat_id": TELEGRAM_CHAT,
-        "text": mensagem,
-        "parse_mode": "HTML"
+        "text": msg
     }
 
-    r = requests.post(url, json=payload)
+    r = requests.post(url, data=payload)
     print("STATUS:", r.status_code)
     print("RESPOSTA:", r.text)
 
